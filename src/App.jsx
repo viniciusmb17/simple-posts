e6a88465c0479942f08bba5e241e4939a21f7d69
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-30 21:22:33 -0300

    Componente Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import { Post } from "./Post"
 
 import './global.css'
 
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Diego Fernandes"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nemo sapiente nobis excepturi minus quis eaque. Cupiditate quasi, libero velit commodi illo dolorem delectus non magni incidunt ea quaerat laboriosam."
-      />
-      <Post 
-        author="Gabriel"
-        content="Quasi, libero velit commodi illo dolorem delectus non magni incidunt ea quaerat laboriosam."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur nemo sapiente nobis excepturi minus quis eaque. Cupiditate quasi, libero velit commodi illo dolorem delectus non magni incidunt ea quaerat laboriosam."
+          />
+          <Post
+            author="Gabriel"
+            content="Quasi, libero velit commodi illo dolorem delectus non magni incidunt ea quaerat laboriosam."
+          />
+        </main>
+      </div>
     </div>
   )
 }
