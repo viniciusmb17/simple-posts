7dce67e155ed16d8a5291ae5f87c8dabde162479
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2022-06-30 21:41:00 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,6 +10,10 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar}
+          src="https://github.com/viniciusmb17.png" 
+        />
         <strong> Vinicius Barbosa</strong>
         <span>Web Developer</span>
       </div>
@@ -16,6 +21,9 @@ export function Sidebar() {
 
       <footer>
         <a href="#">
+          <PencilLine 
+            size={20}
+          />
           Editar seu perfil
         </a>
       </footer>
